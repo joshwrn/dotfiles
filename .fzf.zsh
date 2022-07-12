e94bec827f96b6d4c1d0177a038d3338a572c0c3
Josh Warren <joshnwarren@gmail.com>
2022-07-11 19:59:20 -0700

    make apps install

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == */home/josh/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/josh/.fzf/bin"
+  export PATH="${PATH:+${PATH}:}.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/josh/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source ".fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/josh/.fzf/shell/key-bindings.zsh"
+source ".fzf/shell/key-bindings.zsh"
